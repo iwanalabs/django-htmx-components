183d3d518c35a0438992f7b375821fa4ad3cf6f5
Dylan Castillo <dylanjcastillo@gmail.com>
2024-02-01 10:20:15 +0100

    Disable CSRF validation

--- src/config/settings.py
+++ src/config/settings.py
@@ -47,7 +47,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
+    # "django.middleware.csrf.CsrfViewMiddleware", # don't do this!
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
