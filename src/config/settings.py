a262fec78b3ae01d9565bdd27d3f49f84adfb600
Dylan Castillo <dylanjcastillo@gmail.com>
2024-02-01 10:08:30 +0100

    Update allowed hosts

--- src/config/settings.py
+++ src/config/settings.py
@@ -25,9 +25,7 @@ SECRET_KEY = "django-insecure-7)6m67&xwfifm-a$4q9nr9ygh3=b8t0due-5=*$n*p+&^t-ac&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "*",
-]
+ALLOWED_HOSTS = ["components.iwanalabs.com", "localhost", "127.0.0.1"]
 
 # Application definition
 
