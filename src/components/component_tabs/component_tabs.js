6b649139196e52311a73238bea33d57af14c3862
Dylan Castillo <dylanjcastillo@gmail.com>
2024-02-01 11:22:50 +0100

    Hide other tabs on init

--- src/components/component_tabs/component_tabs.js
+++ src/components/component_tabs/component_tabs.js
@@ -2,9 +2,17 @@ document.addEventListener("DOMContentLoaded", function () {
   const tabsSelect = document.getElementById("tabs");
   const contentTabs = document.getElementsByClassName("tab-content");
 
+  for (let i = 0; i < contentTabs.length; i++) {
+    contentTabs[i].classList.add("hidden");
+  }
+
+  let tabOption = document.getElementById(tabsSelect.value);
+  let tabContent = document.getElementById(tabOption.dataset.target);
+  tabContent.classList.remove("hidden");
+
   tabsSelect.addEventListener("change", function (event) {
-    const tabOption = document.getElementById(event.target.value);
-    const tabContent = document.getElementById(tabOption.dataset.target);
+    tabOption = document.getElementById(event.target.value);
+    tabContent = document.getElementById(tabOption.dataset.target);
 
     for (let i = 0; i < contentTabs.length; i++) {
       contentTabs[i].classList.add("hidden");
