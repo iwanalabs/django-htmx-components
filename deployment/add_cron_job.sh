8dbedfddea5073875e4682677213a141d722775a
Dylan Castillo <dylanjcastillo@gmail.com>
2024-02-02 14:58:08 +0100

    Update script

--- deployment/add_cron_job.sh
+++ deployment/add_cron_job.sh
@@ -3,11 +3,11 @@ set -e
 
 source config.sh
 
-# Define the command to add
 JOB="*/5 * * * * $PROJECT_DIR/.venv/bin/python $PROJECT_DIR/$PROJECT_SUBDIR/manage.py regenerate_data"
 
-# Check if the job already exists
+echo "Adding the following cron job:"
+echo "$JOB"
+
 (crontab -l | grep -q "$JOB") && echo "Cron job already exists" || (crontab -l 2>/dev/null; echo "$JOB") | crontab -
 
-# Confirmation message
 echo "Cron job added successfully"
